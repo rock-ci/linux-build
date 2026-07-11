d22ced53053c9d0766e3a12f66aed8f369e24e26
Hugh Cole-Baker <sigmaris@gmail.com>
2026-07-11 20:05:12 +0100

    Add VP9 support patch

--- configs/rkcombined.sh
+++ configs/rkcombined.sh
@@ -6,8 +6,10 @@ git am -3 "${REPO_ROOT}/patches/rockchip-0054-WIP-2001-dt-bindings-media-Add-Roc
 git am -3 "${REPO_ROOT}/patches/rockchip-0044-WIP-1002-arm64-boot-dts-Increase-ACLK_PERILP0-clock-.patch"
 git am -3 "${REPO_ROOT}/patches/rockchip-0055-WIP-2001-media-rockchip-Add-Rockchip-IEP-driver.patch"
 git am -3 "${REPO_ROOT}/patches/rockchip-0057-WIP-2001-ARM64-dts-rockchip-Add-IEP-node-for-RK3399.patch"
+git am -3 "${REPO_ROOT}/patches/rockchip-0178-WIP-media-rkvdec-Add-VP9-support-for-the-VDPU381-var.patch"
 git am -3 "${REPO_ROOT}/patches/0001-drm-rockchip-rk3399-Support-AFBC-on-all-big-VOP-plan.patch"
 git am -3 "${REPO_ROOT}/patches/0002-sigmaris-dts-rockpro64-Enable-fast-read-for-spiflash.patch"
 git am -3 "${REPO_ROOT}/patches/0001-media-rockchip-rga-Add-support-for-downconverting-fr.patch"
+
 cp "${SCRIPT_DIR}/rockchip.config" arch/arm64/configs
 make defconfig rockchip.config
